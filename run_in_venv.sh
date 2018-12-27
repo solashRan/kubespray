69453128be77cab0dbfce141641eb5c2ea7d5aec
Ran Solash <rans@iguazio.com>
2018-12-27 13:43:37 -0300

    yyyy

--- run_in_venv.sh
+++ run_in_venv.sh
@@ -4,5 +4,5 @@ set -ex
 
 pipenv --two install -r requirements.txt
 pipenv run ./run_tools.py prepare $@
-ansible-playbook -i inventory/igz/hosts.ini cluster.yml -u iguazio -b --skip -tags=igz-online
+pipenv run ansible-playbook -i inventory/igz/hosts.ini cluster.yml -u iguazio -b --skip -tags=igz-online
 pipenv run ./run_tools.py post $@
