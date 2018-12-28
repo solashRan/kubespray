43aa5a294d111dfa542cc2ef16971b6be25999e7
Ran Solash <rans@iguazio.com>
2018-12-28 12:50:46 -0300

    add client set

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 set -ex
 
 ./gen_templates.py $@
-ansible-playbook -i inventory/igz/hosts.ini cluster.yml -u iguazio -b --skip -tags=igz-online
+ansible-playbook -i inventory/igz/hosts.ini cluster.yml -b --skip -tags=igz-online
+ansible-playbook -i inventory/igz/hosts.ini clients.yml -b
